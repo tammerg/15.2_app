36aad729c47750e1557bda5625aa68c6d038765d
tammerg <tg385@scarletmail.rutgers.edu>
2016-02-18 19:48:54 -0500

    got the page up and running with working sequelize and functioning validation"

--- index.js
+++ index.js
@@ -1,84 +1,80 @@
 var express = require('express');
-var expressHandlebars = require('handlebars');
+var expressHandlebars = require('express-handlebars');
 var bodyParser = require('body-parser');
 
 var Sequelize = require('sequelize');
 
-var connection = new Sequelize('test_validation_db', 'root', {
-  dialect: 'mysql',
-  port: 3306,
-  host: 'localhost'
-});
+var connection = new Sequelize('test_validation_db', 'root');
 
 var PORT = 1738;
 
 var app = express();
 
 app.engine('handlebars', expressHandlebars({
-  defaultLayout: 'main'
+	defaultLayout: 'main'
 }));
 
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({
-  extended: false
+	extended: false
 }));
 
 var Note = connection.define('note', {
-  title: {
-    type: Sequelize.STRING,
-    unique: true,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-      len:{
-        args:[1,10],
-        msg: 'please enter a title that is not too long and at least one character',
-      }
-    }
-  },
-  body: {
-    type: Sequelize.TEXT,
-    validate: {
-      check: function(bodyVal){
-        if (bodyVal == "watwatwat") {
-          throw new Error('Text must not be watwatwat')
-        }
-      }
-    }
-  }
+	title: {
+		type: Sequelize.STRING,
+		unique: true,
+		allowNull: false,
+		validate: {
+			notEmpty: true,
+			len: {
+				args: [1,10],
+				msg: 'Please enter a title that is not too long and at least one character',
+			}
+		}
+	},
+	body: {
+		type: Sequelize.TEXT,
+		validate: {
+			check: function(bodyVal) {
+				if (bodyVal == "watwatwat") {
+					throw new Error('Text must not be watwatwat');
+				}
+			}
+		}
+	}
 });
 
-app.get('/', function(req, res){
-  Note.findAll({}).then(function(results){
-    res.render('home', {results});
-  })
+app.get('/', function(req,res) {
+	Note.findAll({}).then(function(results) {
+		res.render('home', {results});
+	});
 });
 
-app.post('/entry', function(req, res){
-  var myTitle = req.body.title;
-  var myText = req.body.text;
+app.post('/entry', function(req, res) {
+	var myTitle = req.body.title;
+	var myText = req.body.text;
 
-  Note.create({
-    title: myTitle,
-    body: myTet
-  }).then(function({
-    res.redirect('/success')
-  }).catch(function(err){
-    console.log(err);
-    res.redirect('/fail');
-  })
+	Note.create({
+		title: myTitle,
+		body: myText
+	}).then(function(result) {
+		res.redirect('/success');
+	}).catch(function(err) {
+		console.log(err);
+		res.redirect('/fail');
+	});
 });
 
-app.get('/success', function(req, res){
-  res.send('Sucess Page!');
+app.get('/success', function(req, res) {
+	res.send('Success Page!');
 });
 
-app.get('/fail', function(req, res){
-  res.send("failed to add entry.");
+app.get('/fail', function(req, res) {
+	res.send('Fail to add entry!');
 });
 
-connecton.sync().then(function(){
-  app.listen(PORT, function({
-    console.log('listening on port 1738');
-  })
+connection.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log("Listening on port %s", PORT);
+	});
 });
